616e275affd7cd9713ab10cd256f44cae7087818
Marko Pietarinen <marko.pietarinen@fmi.fi>
2020-05-14 14:36:21 +0300

    NFmiTimeDescriptor::GetIntersection must be const (merging by hand lost it somehow).

--- newbase/NFmiTimeDescriptor.cpp
+++ newbase/NFmiTimeDescriptor.cpp
@@ -1183,7 +1183,7 @@ void NFmiTimeDescriptor::SetLocalTimes(const float theLongitude)
  */
 // ----------------------------------------------------------------------
 NFmiTimeDescriptor NFmiTimeDescriptor::GetIntersection(const NFmiMetTime &theStartLimit,
-                                                       const NFmiMetTime &theEndLimit)
+                                                       const NFmiMetTime &theEndLimit) const
 {
   NFmiTimeDescriptor timedesc(*this);
   if (itsTimeBagIdent)
