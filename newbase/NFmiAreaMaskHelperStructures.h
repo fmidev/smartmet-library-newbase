66b8be6e04bd360dcbbdba7ca6e2edb0c7b2a1d2
Marko Pietarinen <mpietarin@gmail.com>
2024-01-23 12:13:42 +0200

    1. NFmiMacroParamValue class now has support for time-serial based smarttools calculations. 2. NFmiCalculationParams has support for: - special calculations (cross-section and time-serial). - CalculationPoint type macroParam calculations (new member data itsActualCalculationPoint and new UsedLatlon and SetModifiedLatlon methods). - Support for multi-data cases, like synop data has three queryData (Finland, Europe and World) with itsCurrentMultiInfoData data member. - Support for observation max distance limiting (itsObservationRadiusInKm data member)

--- newbase/NFmiAreaMaskHelperStructures.h
+++ newbase/NFmiAreaMaskHelperStructures.h
@@ -3,6 +3,8 @@
 #include "NFmiMetTime.h"
 #include "NFmiPoint.h"
 
+class NFmiFastQueryInfo;
+
 class NFmiMacroParamValue
 {
  public:
@@ -17,6 +19,7 @@ class NFmiMacroParamValue
   // Tämä olio on käytössä vain jos tämä on true
   bool fSetValue = false;
   bool fDoCrossSectionCalculations = false;
+  bool fDoTimeSerialCalculations = false;
 };
 
 class NFmiCalculationParams
@@ -27,17 +30,39 @@ class NFmiCalculationParams
                         unsigned long theLocationIndex,
                         const NFmiMetTime &theTime,
                         unsigned long theTimeIndex,
-                        bool crossSectionCase = false,
+                        bool specialCalculation = false,
                         float thePressureHeight = kFloatMissing);
 
+  const NFmiPoint &UsedLatlon(bool forceCalculationGridPoint = false) const;
+  void SetModifiedLatlon(const NFmiPoint &modifiedLatlon, bool setUseModifiedFlag);
+
+  // Tässä on yleensä macroParamin laskentapisteen arvo, jota käytetään laskennoissa.
   NFmiPoint itsLatlon;
   unsigned long itsLocationIndex = gMissingIndex;
   NFmiMetTime itsTime;
   unsigned long itsTimeIndex = gMissingIndex;
-  // Joskus pitää tietää että kyse on poikkileikkaus laskuista
-  bool fCrossSectionCase = false;
+  // Joskus pitää tietää että kyse on poikkileikkaus tai aikasarja laskuista
+  bool fSpecialCalculationCase = false;
   // Silloin (poikkileikkauslaskuissa) käytetään tätä painepintaa laskentapisteenä
   // Tein siitä mutablen, jotta ei tarvitse muuttaa niin monissa kohtaa const parametria ei
   // const:iksi.
   mutable float itsPressureHeight = kFloatMissing;
+  // Jos laskuissa on käytetty CalculationPoint = synop tyyliin laskenta pisteitä, tähän talletetaan
+  // sen originaali latlon pisteen pointteri, jota sitten voidaan käyttää tarkemmissa
+  // etäisyyslaskuissa, sen sijaan että käytettäisiin laskentahilan lähimmän pisteen koordinaatteja.
+  // Tämä estää tulosten satunnaisen oloisen vaihtelun, kun laskenta hila muuttaa ja etäisyys
+  // perusteiset laskennan tulokset muuttuvat (esim. etäisyydet occurance laskentojen hakusäteen
+  // suhteen).
+  const NFmiPoint *itsActualCalculationPoint = nullptr;
+  // Synop datoilla voi olla monia eri datoja (esim. Suomessa suomi+euro+maailma datat).
+  // Ainakin SimpleCondition laskuihin näitä olisi muuten vaikea saada käyttöön, joten tässä on
+  // 'kuljetus' laskenta paikalle.
+  mutable NFmiFastQueryInfo *itsCurrentMultiInfoData = nullptr;
+  // Joskus havaintoasemien maksimi sallittua etäisyyttä laskentapisteeseen halutaan
+  // rajoittaa smarttool kielessä halutulla km määrällä "observationradius = 5",
+  // tällöin jos asema on kauempana, sitä ei käytetä ollenkaan kyseiselle laskenta pisteelle.
+  float itsObservationRadiusInKm = kFloatMissing;
+  // Joskus ollaan muutettu itsLatlon muuttujan arvoa tahallaan (peekxy jms funktiot),
+  // ja tällöin halutaan käyttää sitä vaikka kyse olisi calculationpoint laskennasta.
+  bool fUseModifiedLatlon = false;
 };
